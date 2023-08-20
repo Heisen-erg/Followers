89288e1ca325a236e4cdfc07288e5b7a5a580146
Heisen-erg <heisenberg.database.1@gmail.com>
2023-08-20 20:02:28 +0530

    r

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -10,10 +10,10 @@ const Home = () => {
     <div className="container-fluid">
       <div className="row">
 
-        <div className="col-12 col-xl-6 m-0 p-0 custom z-2 d-grid"><motion.div initial={{ opacity: 0,x:30} }whileInView={{ opacity: 1,x:0 }}transition={{ duration: 3 }} useElementScroll={true} className='d-grid' >   <img src={require('../assets/instgram3.jpg')} alt="" className='image' /> </motion.div>   </div>
+        <div className="col-12 col-xl-6 m-0 p-0 custom z-2 d-grid"><motion.div initial={{ opacity: 0,y:30} }whileInView={{ opacity: 1,y:0 }}transition={{ duration: 3 }} useElementScroll={true} className='d-grid z-3' >   <img src={require('../assets/instgram3.jpg')} alt="" className='image' /> </motion.div>   </div>
         <div className="col-12 col-xl-6 m-0 p-0 custom z-2 d-grid "> <div className="orange"></div> <h1 className='text' > <span style={{ color: 'rgb(0, 16, 100)' }}> <Typewriter options={{ autoStart: true, loop: true, delay: 80, strings: ["Want  Instagram Followers ?"] }} />   </span> <br /> <br /> <br /> You Can Get Here ,Instagram Followers At Most affordable Cost. </h1> </div>
         <div className="col-12 col-xl-6 m-0 p-0 custom z-2 d-grid order-1 order-xl-0 " > <span className='stats'><Countup enableScrollSpy={true} start={0} end={240} duration={4}   > </Countup> + <br /> CUSTOMERS </span>   </div>
-         <div className="col-12 col-xl-6 m-0 p-0 custom z-2 d-grid order-0 order-xl-1 ">  <motion.div initial={{ opacity: 0,x:-30} }whileInView={{ opacity: 1,x:0 }}transition={{ duration: 1 }} useElementScroll={true} className='d-grid' >  <img src={require('../assets/instgram2.jpg')} alt="" className='image' /> </motion.div>  </div> 
+         <div className="col-12 col-xl-6 m-0 p-0 custom z-2 d-grid order-0 order-xl-1 ">  <motion.div initial={{ opacity: 0,y:30} }whileInView={{ opacity: 1,y:0 }}transition={{ duration: 1 }} useElementScroll={true} className='d-grid z-3' >  <img src={require('../assets/instgram2.jpg')} alt="" className='image' /> </motion.div>  </div> 
 
 
       </div>
