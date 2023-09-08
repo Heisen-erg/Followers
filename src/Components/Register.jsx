76821fe1df65a517ebe517a112f4fc7b0ad2923a
Heisen-erg <heisenberg.database.1@gmail.com>
2023-09-08 16:59:20 +0530

    hh

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -26,7 +26,7 @@ const [pass,setpass] = useState({});
 // else if(Rishi === conRishi){
   
   // https://rishi-server.vercel.app/user/register
-  try{
+ 
   axios.post("https://rishi-server.vercel.app/plans/register",pass,{headers:{
   "Content-Type":'application/json'
 }}).then(({data})=>{toast({
@@ -40,9 +40,7 @@ const [pass,setpass] = useState({});
 // document.getElementById('Value5').value=""
 // document.getElementById('Value6').value=""
 })
- }
- catch (error) {
-  console.log(error);}
+
 // }
  }
 
