675bc3ffa5800f3ffd0991b3c97f5bf5a170ba04
Heisen-erg <heisenberg.database.1@gmail.com>
2023-09-08 19:20:31 +0530

    hh

--- src/Components/Usertable.jsx
+++ src/Components/Usertable.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Usertable = ({userrishi}) => {
   return (
-  <td className='text-center'style={{color:"grey",margin:'1vh 0vh 1vh 0vh'}}>
-{userrishi}
-  </td>
+  <tr style={{height:'50px',fontFamily:'dancing',fontSize:"1vmax",fontWeight:'700'}} > <td className='text-center'style={{color:"grey",margin:'1vh 0vh 1vh 0vh'}}> {userrishi} </td> </tr>
+
+ 
   )
 }
 
