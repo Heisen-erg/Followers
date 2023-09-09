4e5234102f2db9fe654240f2057fbc75376267fb
Heisen-erg <heisenberg.database.1@gmail.com>
2023-09-09 12:43:45 +0530

    hh

--- src/Components/Pricing.jsx
+++ src/Components/Pricing.jsx
@@ -26,7 +26,7 @@ useEffect(() => {
 
     <table style={{marginTop:'7vh',fontFamily:'cursive',fontSize:'2vmax',color:'rgb(107, 43, 43)', border: '5px solid black'}}>
 
-<tr >
+<tr  style={{ border: '5px solid black' }}>
   <th style={{width:'25vw', border: '5px solid black' }}  className='text-center'>PLAN</th>
   <th style={{width:'50vw', border: '5px solid black'}} className='text-center'>DESCRIPTION</th>
   <th style={{width:'25vw', border: '5px solid black'}}  className='text-center'>PRICE</th>
